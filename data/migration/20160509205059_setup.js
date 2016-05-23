f13215a36cba44b3481fecf4022a53470bf3948c
Rowan Manning <info@rowanmanning.com>
2016-05-23 19:39:52 +0100

    Add a Travis config

--- data/migration/20160509205059_setup.js
+++ data/migration/20160509205059_setup.js
@@ -5,41 +5,44 @@
 // the alpha with a clean initial database setup.
 
 exports.up = (database, Promise) => {
-	return Promise.join(
+	return Promise.resolve()
+		.then(() => {
+			// Create the sites table
+			return database.schema.createTable('sites', table => {
 
-		// Create the sites table
-		database.schema.createTable('sites', table => {
+				// Sites table columns
+				table.uuid('id').unique().primary();
+				table.string('name').notNullable();
+				table.timestamp('created_at').defaultTo(database.fn.now());
+				table.timestamp('updated_at').defaultTo(database.fn.now());
 
-			// Sites table columns
-			table.uuid('id').unique().primary();
-			table.string('name').notNullable();
-			table.timestamp('created_at').defaultTo(database.fn.now());
-			table.timestamp('updated_at').defaultTo(database.fn.now());
-
-		}),
-
-		// Create the URLs table
-		database.schema.createTable('urls', table => {
-
-			// URLs table columns
-			table.uuid('id').unique().primary();
-			table.uuid('site');
-			table.string('name').notNullable();
-			table.string('address').notNullable();
-			table.timestamp('created_at').defaultTo(database.fn.now());
-			table.timestamp('updated_at').defaultTo(database.fn.now());
-
-			// Foreign key contraints
-			table.foreign('site').references('sites.id');
-
+			});
 		})
+		.then(() => {
+			// Create the URLs table
+			return database.schema.createTable('urls', table => {
 
-	);
+				// URLs table columns
+				table.uuid('id').unique().primary();
+				table.uuid('site');
+				table.string('name').notNullable();
+				table.string('address').notNullable();
+				table.timestamp('created_at').defaultTo(database.fn.now());
+				table.timestamp('updated_at').defaultTo(database.fn.now());
+
+				// Foreign key contraints
+				table.foreign('site').references('sites.id');
+
+			});
+		});
 };
 
 exports.down = (database, Promise) => {
-	return Promise.join(
-		database.schema.dropTable('urls'),
-		database.schema.dropTable('sites')
-	);
+	return Promise.resolve()
+		.then(() => {
+			return database.schema.dropTable('urls');
+		})
+		.then(() => {
+			return database.schema.dropTable('sites');
+		});
 };
