42a5a327c1ebd9d33b93f6d8188a84cfafb75ecd
Rowan Manning <info@rowanmanning.com>
2016-11-22 22:10:49 +0000

    Add comments explaining how middleware works

--- middleware/handle-errors.js
+++ middleware/handle-errors.js
@@ -5,12 +5,20 @@ module.exports = {
 	json: handleErrorsJson
 };
 
+// This middleware is mounted after everything else as
+// an error handler. This responds with HTML errors
 function handleErrorsHtml(dashboard) {
 	return (error, request, response, next) => {
 		// jshint unused:false
 		const renderContext = buildRenderContext(dashboard, error);
 
+		// First attempt to render the "error" view, which
+		// can be found in view/error.dust
 		response.render('error', renderContext, (renderError, html) => {
+
+			// If the render fails, we build some simple
+			// backup HTML instead. Otherwise we send the
+			// rendered HTML
 			if (renderError) {
 				html = buildBackupHtml(dashboard, {
 					error,
@@ -21,12 +29,16 @@ function handleErrorsHtml(dashboard) {
 			response.status(renderContext.error.status).send(html);
 		});
 
+		// Output server errors in the logs – we need
+		// to know about these
 		if (renderContext.error.status >= 500) {
 			dashboard.log.error(error.stack);
 		}
 	};
 }
 
+// This middleware is mounted after everything else as
+// an error handler. This responds with JSON errors
 function handleErrorsJson(dashboard) {
 	return (error, request, response, next) => {
 		// jshint unused:false
@@ -40,6 +52,8 @@ function handleErrorsJson(dashboard) {
 	};
 }
 
+// This method builds the render context for an error
+// page, or error JSON
 function buildRenderContext(dashboard, error) {
 	const statusCode = (error.status && error.status >= 400 ? error.status : 500);
 	return {
@@ -51,6 +65,9 @@ function buildRenderContext(dashboard, error) {
 	};
 }
 
+// This function builds backup HTML for if Dust rendering
+// breaks for some reason. We output both the original
+// error and the error that occurred during rendering
 function buildBackupHtml(dashboard, data) {
 	let errorStack;
 	let renderErrorStack;
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -4,6 +4,9 @@ const httpError = require('http-errors');
 
 module.exports = notFound;
 
+// This middleware can be mounted after all of the
+// other routes in order to pass on 404 errors to
+// the main error handlers
 function notFound(request, response, next) {
 	next(httpError(404));
 }
