227c97a8b4cc8936f7f1e596f4b62bc05dc7d09d
Rowan Manning <rowan.manning@ft.com>
2017-03-02 09:58:23 +0000

    Add API read methods for sites and URLs

--- data/seed/github-website.js
+++ data/seed/github-website.js
@@ -3,36 +3,24 @@
 // Note: IDs in here are referenced in the integration
 // tests, so should not be changed.
 exports.seed = (database, Promise) => {
-	const siteId = 's01p_site';
+	const siteId = 's02g_site';
 
 	return Promise.resolve()
 		.then(() => {
 			// Add a site
 			return database('sites').insert({
 				id: siteId,
-				name: 'Pa11y'
+				name: 'GitHub'
 			});
 		})
 		.then(() => {
 			// Add a bunch of URLs to the site
 			return database('urls').insert([
 				{
-					id: 's01p_u01h',
+					id: 's02g_u01h',
 					site: siteId,
 					name: 'Home',
-					address: 'http://pa11y.github.io/'
-				},
-				{
-					id: 's01p_u02c',
-					site: siteId,
-					name: 'Contact',
-					address: 'http://pa11y.github.io/contact/'
-				},
-				{
-					id: 's01p_u03c',
-					site: siteId,
-					name: 'Contributing',
-					address: 'http://pa11y.github.io/contributing/'
+					address: 'https://github.com/'
 				}
 			]);
 		});
--- test/integration/api-v1.js
+++ test/integration/api-v1.js
@@ -64,11 +64,12 @@ describe('GET /api/v1/sites', () => {
 	it('responds with all of the sites in the database as an array', done => {
 		dashboard.database.select('*').from('sites')
 			.then(sites => {
-				const jsonifiedSites = JSON.parse(JSON.stringify(sites));
+				const jsonifiedSites = JSON.parse(JSON.stringify(sites))
+					.map(dashboard.model.site.prepareForOutput);
 				request.expect(response => {
 					assert.isObject(response.body);
 					assert.isArray(response.body.sites);
-					assert.greaterThan(response.body.length, 0);
+					assert.greaterThan(response.body.sites.length, 0);
 					assert.deepEqual(response.body.sites, jsonifiedSites);
 				}).end(done);
 			})
@@ -81,14 +82,9 @@ describe('GET /api/v1/sites/:siteId', () => {
 	let request;
 	let siteId;
 
-	beforeEach(done => {
-		dashboard.database.select('*').from('sites')
-			.then(sites => {
-				siteId = sites[0].id;
-				request = agent.get(`/api/v1/sites/${siteId}`);
-				done();
-			})
-			.catch(done);
+	beforeEach(() => {
+		siteId = 's01p_site';
+		request = agent.get(`/api/v1/sites/${siteId}`);
 	});
 
 	it('responds with a 200 status', done => {
@@ -102,7 +98,8 @@ describe('GET /api/v1/sites/:siteId', () => {
 	it('responds with the site as an object', done => {
 		dashboard.database.select('*').from('sites').where({id: siteId})
 			.then(sites => {
-				const jsonifiedSite = JSON.parse(JSON.stringify(sites[0]));
+				const jsonifiedSite = dashboard.model.site
+					.prepareForOutput(JSON.parse(JSON.stringify(sites[0])));
 				request.expect(response => {
 					assert.isObject(response.body);
 					assert.isObject(response.body.site);
@@ -110,6 +107,165 @@ describe('GET /api/v1/sites/:siteId', () => {
 				}).end(done);
 			})
 			.catch(done);
+	});
+
+	describe('when a site with the given ID does not exist', () => {
+
+		beforeEach(() => {
+			siteId = 'notasite';
+			request = agent.get(`/api/v1/sites/${siteId}`);
+		});
+
+		it('responds with a 404 status', done => {
+			request.expect(404).end(done);
+		});
+
+	});
+
+});
+
+describe('GET /api/v1/sites/:siteId/urls', () => {
+	let request;
+	let siteId;
+
+	beforeEach(() => {
+		siteId = 's01p_site';
+		request = agent.get(`/api/v1/sites/${siteId}/urls`);
+	});
+
+	it('responds with a 200 status', done => {
+		request.expect(200).end(done);
+	});
+
+	it('responds with JSON', done => {
+		request.expect('Content-Type', 'application/json; charset=utf-8').end(done);
+	});
+
+	it('responds with all of the urls in the database for the given site as an array', done => {
+		dashboard.database.select('*').from('urls').where({site: siteId})
+			.then(urls => {
+				const jsonifiedUrls = JSON.parse(JSON.stringify(urls))
+					.map(dashboard.model.url.prepareForOutput);
+				request.expect(response => {
+					assert.isObject(response.body);
+					assert.isArray(response.body.urls);
+					assert.greaterThan(response.body.urls.length, 0);
+					assert.deepEqual(response.body.urls, jsonifiedUrls);
+				}).end(done);
+			})
+			.catch(done);
+	});
+
+	describe('when the site has no URLs', () => {
+
+		beforeEach(() => {
+			siteId = 's03e_site';
+			request = agent.get(`/api/v1/sites/${siteId}/urls`);
+		});
+
+		it('responds with a 200 status', done => {
+			request.expect(200).end(done);
+		});
+
+		it('responds with JSON', done => {
+			request.expect('Content-Type', 'application/json; charset=utf-8').end(done);
+		});
+
+		it('responds with an empty array', done => {
+			request.expect(response => {
+				assert.isObject(response.body);
+				assert.isArray(response.body.urls);
+				assert.strictEqual(response.body.urls.length, 0);
+			}).end(done);
+		});
+
+	});
+
+	describe('when a site with the given ID does not exist', () => {
+
+		beforeEach(() => {
+			siteId = 'notasite';
+			request = agent.get(`/api/v1/sites/${siteId}/urls`);
+		});
+
+		it('responds with a 404 status', done => {
+			request.expect(404).end(done);
+		});
+
+	});
+
+});
+
+describe('GET /api/v1/sites/:siteId/urls/:urlId', () => {
+	let request;
+	let siteId;
+	let urlId;
+
+	beforeEach(() => {
+		siteId = 's01p_site';
+		urlId = 's01p_u02c';
+		request = agent.get(`/api/v1/sites/${siteId}/urls/${urlId}`);
+	});
+
+	it('responds with a 200 status', done => {
+		request.expect(200).end(done);
+	});
+
+	it('responds with JSON', done => {
+		request.expect('Content-Type', 'application/json; charset=utf-8').end(done);
+	});
+
+	it('responds with the url as an object', done => {
+		dashboard.database.select('*').from('urls').where({id: urlId})
+			.then(urls => {
+				const jsonifiedUrl = dashboard.model.url
+					.prepareForOutput(JSON.parse(JSON.stringify(urls[0])));
+				request.expect(response => {
+					assert.isObject(response.body);
+					assert.isObject(response.body.url);
+					assert.deepEqual(response.body.url, jsonifiedUrl);
+				}).end(done);
+			})
+			.catch(done);
+	});
+
+	describe('when a site with the given ID does not exist', () => {
+
+		beforeEach(() => {
+			siteId = 'notasite';
+			request = agent.get(`/api/v1/sites/${siteId}/urls/${urlId}`);
+		});
+
+		it('responds with a 404 status', done => {
+			request.expect(404).end(done);
+		});
+
+	});
+
+	describe('when a url with the given ID does not exist', () => {
+
+		beforeEach(() => {
+			urlId = 'notaurl';
+			request = agent.get(`/api/v1/sites/${siteId}/urls/${urlId}`);
+		});
+
+		it('responds with a 404 status', done => {
+			request.expect(404).end(done);
+		});
+
+	});
+
+	describe('when the site ID and url ID are mismatched', () => {
+
+		beforeEach(() => {
+			urlId = 's02g_u01h';
+			request = agent.get(`/api/v1/sites/${siteId}/urls/${urlId}`);
+		});
+
+		it('responds with a 404 status', done => {
+			request.expect(404).end(done);
+		});
+
 	});
 
 });
