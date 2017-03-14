6d72516e2b73a963f51b04b3700cc4c48625c419
Rowan Manning <rowan.manning@ft.com>
2017-03-14 10:34:00 +0000

    Add an example of using models in front end routes

--- controller/api-v1.js
+++ controller/api-v1.js
@@ -24,7 +24,7 @@ module.exports = dashboard => {
 			.then(siteId => {
 				response.set('Location', `/api/v1/sites/${siteId}`);
 				response.status(201);
-				response.send({});
+				response.send(response.locals);
 			})
 			.catch(error => {
 				if (error.isValidationError) {
@@ -38,21 +38,21 @@ module.exports = dashboard => {
 	app.get('/api/v1/sites', (request, response, next) => {
 		model.site.getAll()
 			.then(sites => {
-				response.send({sites});
+				response.locals.sites = sites;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
 
 	// Get a site by ID
 	app.get('/api/v1/sites/:siteId', (request, response, next) => {
-		const json = {};
 		model.site.getById(request.params.siteId)
 			.then(site => {
 				if (!site) {
 					throw httpError(404);
 				}
-				json.site = site;
-				response.send(json);
+				response.locals.site = site;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
@@ -68,7 +68,7 @@ module.exports = dashboard => {
 			})
 			.then(() => {
 				response.set('Location', `/api/v1/sites/${request.params.siteId}`);
-				response.send({});
+				response.send(response.locals);
 			})
 			.catch(error => {
 				if (error.isValidationError) {
@@ -88,7 +88,8 @@ module.exports = dashboard => {
 				return model.site.delete(site.id);
 			})
 			.then(deleted => {
-				response.send({deleted});
+				response.locals.deleted = deleted;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
@@ -106,7 +107,7 @@ module.exports = dashboard => {
 			.then(urlId => {
 				response.set('Location', `/api/v1/sites/${request.params.siteId}/urls/${urlId}`);
 				response.status(201);
-				response.send({});
+				response.send(response.locals);
 			})
 			.catch(error => {
 				if (error.isValidationError) {
@@ -118,7 +119,6 @@ module.exports = dashboard => {
 
 	// Get a site's URLs
 	app.get('/api/v1/sites/:siteId/urls', (request, response, next) => {
-		const json = {};
 		model.site.getById(request.params.siteId)
 			.then(site => {
 				if (!site) {
@@ -127,15 +127,14 @@ module.exports = dashboard => {
 				return model.url.getAllBySite(site.id);
 			})
 			.then(urls => {
-				json.urls = urls;
-				response.send(json);
+				response.locals.urls = urls;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
 
 	// Get a site's results
 	app.get('/api/v1/sites/:siteId/results', (request, response, next) => {
-		const json = {};
 		model.site.getById(request.params.siteId)
 			.then(site => {
 				if (!site) {
@@ -144,22 +143,21 @@ module.exports = dashboard => {
 				return model.result.getAllBySite(site.id);
 			})
 			.then(results => {
-				json.results = results;
-				response.send(json);
+				response.locals.results = results;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
 
 	// Get a URL by ID
 	app.get('/api/v1/sites/:siteId/urls/:urlId', (request, response, next) => {
-		const json = {};
 		model.url.getByIdAndSite(request.params.urlId, request.params.siteId)
 			.then(url => {
 				if (!url) {
 					throw httpError(404);
 				}
-				json.url = url;
-				response.send(json);
+				response.locals.url = url;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
@@ -175,7 +173,7 @@ module.exports = dashboard => {
 			})
 			.then(() => {
 				response.set('Location', `/api/v1/sites/${request.params.siteId}/urls/${request.params.urlId}`);
-				response.send({});
+				response.send(response.locals);
 			})
 			.catch(error => {
 				if (error.isValidationError) {
@@ -195,14 +193,14 @@ module.exports = dashboard => {
 				return model.url.delete(url.id);
 			})
 			.then(deleted => {
-				response.send({deleted});
+				response.locals.deleted = deleted;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
 
 	// Get a URL's results
 	app.get('/api/v1/sites/:siteId/urls/:urlId/results', (request, response, next) => {
-		const json = {};
 		model.url.getByIdAndSite(request.params.urlId, request.params.siteId)
 			.then(url => {
 				if (!url) {
@@ -211,22 +209,21 @@ module.exports = dashboard => {
 				return model.result.getAllByUrl(request.params.urlId);
 			})
 			.then(results => {
-				json.results = results;
-				response.send(json);
+				response.locals.results = results;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
 
 	// Get a result by ID
 	app.get('/api/v1/sites/:siteId/urls/:urlId/results/:resultId', (request, response, next) => {
-		const json = {};
 		model.result.getByIdAndUrlAndSite(request.params.resultId, request.params.urlId, request.params.siteId)
 			.then(result => {
 				if (!result) {
 					throw httpError(404);
 				}
-				json.result = result;
-				response.send(json);
+				response.locals.result = result;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
@@ -241,7 +238,8 @@ module.exports = dashboard => {
 				return model.result.delete(result.id);
 			})
 			.then(deleted => {
-				response.send({deleted});
+				response.locals.deleted = deleted;
+				response.send(response.locals);
 			})
 			.catch(next);
 	});
--- controller/front-end.js
+++ controller/front-end.js
@@ -1,14 +1,38 @@
 'use strict';
 
 const handleErrors = require('../middleware/handle-errors');
+const httpError = require('http-errors');
 const notFound = require('../middleware/not-found');
 
 module.exports = dashboard => {
 	const app = dashboard.app;
+	const model = dashboard.model;
 
 	// Home page
-	app.get('/', (request, response) => {
-		response.render('index');
+	app.get('/', (request, response, next) => {
+		model.site.getAll()
+			.then(sites => {
+				response.locals.sites = sites;
+				response.render('index');
+			})
+			.catch(next);
+	});
+
+	// Site page
+	app.get('/sites/:siteId', (request, response, next) => {
+		model.site.getById(request.params.siteId)
+			.then(site => {
+				if (!site) {
+					throw httpError(404);
+				}
+				response.locals.site = site;
+				return model.url.getAllBySite(site.id);
+			})
+			.then(urls => {
+				response.locals.urls = urls;
+				response.render('site');
+			})
+			.catch(next);
 	});
 
 	// API page (redirect to latest version)
